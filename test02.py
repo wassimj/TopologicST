b14692361943fe6d7b317acfa1faef6b6d6b37cc
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-08-24 09:05:35 +0300

    Create test02.py
    Simpler test that just uses topologic

---
+++ test02.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+
+import sys
+import os, re
+from sys import platform
+
+if platform == 'win32':
+    os_name = 'windows'
+else:
+    os_name = 'linux'
+
+sitePackagesFolderName = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin", os_name)
+topologicFolderName = [filename for filename in os.listdir(sitePackagesFolderName) if filename.startswith("topologic")][0]
+topologicPath = os.path.join(sitePackagesFolderName, topologicFolderName)
+sys.path.append(topologicPath)
+topologicPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "site-packages")
+sys.path.append(topologicPath)
+
+import topologic
+
+v1 = topologic.Vertex.ByCoordinates(0,0,0)
+st.write("V1: Created a Topologic Vertex at", v1.X(), v1.Y(), v1.Z())
+v2 = topologic.Vertex.Bycoordinates(10,0,0)
+st.write("V2: Created a Topologic Vertex at", v2.X(), v2.Y(), v2.Z())
+e1 = topologic.Edge.ByStartVertexEndVertex(v1, v2)
+st.write("E1: Connected V1 and V2 with a Toplogic Edge")
+c1 = e1.Centroid()
+st.write("C1: The centroid of the Topologic Edge is at", c1.X(), c1.Y(), c1.Z())
+
