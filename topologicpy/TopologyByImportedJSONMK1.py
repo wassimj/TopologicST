2a1bdc92176579bb3119392e56503d1ecc37f0b2
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-08-27 16:12:17 +0300

    more debugging

--- topologicpy/TopologyByImportedJSONMK1.py
+++ topologicpy/TopologyByImportedJSONMK1.py
@@ -1,21 +1,6 @@
-import bpy
-from bpy.props import IntProperty, FloatProperty, StringProperty
-from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import updateNode
 
 import topologic
-from topologic import Vertex, Edge, Wire, Face, Shell, Cell, CellComplex, Cluster, Topology
 import json
-
-# From https://stackabuse.com/python-how-to-flatten-list-of-lists/
-def flatten(element):
-	returnList = []
-	if isinstance(element, list) == True:
-		for anItem in element:
-			returnList = returnList + flatten(anItem)
-	else:
-		returnList = [element]
-	return returnList
 
 def relevantSelector(topology, tol):
 	returnVertex = None
@@ -383,32 +368,4 @@ def processItem(item):
 			topologies.append(topology)
 		return topologies
 	return None
-		
-class SvTopologyByImportedJSONMK1(bpy.types.Node, SverchCustomTreeNode):
-	"""
-	Triggers: Topologic
-	Tooltip: Creates a Topology from the input BREP file
-	"""
-	bl_idname = 'SvTopologyByImportedJSONMK1'
-	bl_label = 'Topology.ByImportedJSON MK1'
-	FilePath: StringProperty(name="file", default="", subtype="FILE_PATH")
 
-	def sv_init(self, context):
-		self.inputs.new('SvStringsSocket', 'File Path').prop_name='FilePath'
-		self.outputs.new('SvStringsSocket', 'Topology')
-
-	def process(self):
-		if not any(socket.is_linked for socket in self.outputs):
-			return
-		inputs = self.inputs['File Path'].sv_get(deepcopy=False)
-		inputs = flatten(inputs)
-		outputs = []
-		for anInput in inputs:
-			outputs.append(processItem(anInput))
-		self.outputs['Topology'].sv_set(outputs)
-
-def register():
-	bpy.utils.register_class(SvTopologyByImportedJSONMK1)
-
-def unregister():
-	bpy.utils.unregister_class(SvTopologyByImportedJSONMK1)
