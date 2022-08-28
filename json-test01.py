1549cdbbb1d78cd3cb8614b05e0a0eea7b7eb1e2
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-08-28 10:28:44 +0300

    Enhancing UI

--- json-test01.py
+++ json-test01.py
@@ -188,8 +188,8 @@ with input_column:
 
             fig = go.Figure(data=dataList)
             fig.update_layout(
-                width=800,
-                height=800,
+                width=500,
+                height=500,
                 scene = dict(
                     xaxis = dict(visible=False),
                     yaxis = dict(visible=False),
@@ -241,6 +241,6 @@ with input_column:
         # 3D VIEWER
         with viewer_column:
             st.subheader("3D View")
-            st.plotly_chart(fig, width=800,height=800)
+            st.plotly_chart(fig, width=500,height=500)
 
 
