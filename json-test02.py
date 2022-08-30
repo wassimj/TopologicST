91533bf26157f816eaa1a385d30cacc9894d9ac6
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-08-30 10:17:22 +0300

    Updated total to not show

--- json-test02.py
+++ json-test02.py
@@ -334,7 +334,7 @@ with input_column:
             else:
                 sw_ap_or = 0
 
-            total_ap_or_percent = n_ap_or+s_ap_or+e_ap_or+w_ap_or+ne_ap_or+nw_ap_or+se_ap_or+sw_ap_or
+            total_ap_or_percent = 0
             total_project_wall_area = s_wall_area + n_wall_area + ne_wall_area + nw_wall_area + sw_wall_area + se_wall_area
             total_project_aperture_area = n_aperture_area + s_aperture_area + e_aperture_area + w_aperture_area + ne_aperture_area + nw_aperture_area + se_aperture_area + sw_aperture_area
             
@@ -376,7 +376,7 @@ with input_column:
                                        round(nw_ap_or,2),
                                        round(se_ap_or,2),
                                        round(sw_ap_or,2),
-                                       round(total_ap_or_percent,2)],
+                                       "--"],
                 'WWR By Project': [round(n_ap_proj,2),
                                    round(s_ap_proj,2),
                                    round(e_ap_proj,2),
