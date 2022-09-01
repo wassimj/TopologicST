a902dbac94cf067997acb47e3ea53cc4fce24809
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-01 11:19:26 +0300

    Debugging

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.write("""
 parent_path = pathlib.Path(__file__).parent.parent.resolve()
 st.write(parent_path)
 #data_path = os.path.join(parent_path, "data")
-onlyfiles = [f for f in listdir(data_path) if isfile(join(parent_path, f))]
+onlyfiles = [f for f in listdir(parent_path) if isfile(join(parent_path, f))]
 option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
-file_location=os.path.join(data_path, option)
+file_location=os.path.join(parent_path, option)
 # use `file_location` as a parameter to the main script
