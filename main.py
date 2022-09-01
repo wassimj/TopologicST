10060e0dc7c9d702377f578ac71ff2bd9689b90e
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-01 11:18:27 +0300

    bug fix

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ st.write("""
 # Demo
 """)
 parent_path = pathlib.Path(__file__).parent.parent.resolve()
-data_path = os.path.join(parent_path, "data")
-onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+st.write(parent_path)
+#data_path = os.path.join(parent_path, "data")
+onlyfiles = [f for f in listdir(data_path) if isfile(join(parent_path, f))]
 option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
 file_location=os.path.join(data_path, option)
 # use `file_location` as a parameter to the main script
