9fc5d8c3b1eed4f4ef53dd850592f0a27a77cbbc
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-02 17:26:13 +0300

    Debugging

--- pyvista/pv-test03.py
+++ pyvista/pv-test03.py
@@ -2,5 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 html_file = st.file_uploader("", type="html", accept_multiple_files=False)
-html = html_file.read()
-st.components.v1.html(html,height=400)
+if html_file:
+    html = html_file.read()
+    st.markdown(html)
+    st.components.v1.html(html,height=400)
