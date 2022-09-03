1be28803f6b75419272cb3ff6f360d1c5c766d2b
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-03 16:02:28 +0300

    Optimizing

--- json-test02.py
+++ json-test02.py
@@ -228,7 +228,7 @@ if not c:
     if json_file:
         topologies = TopologyByImportedJSONMK1.processItem(json_file)
         c = topologies[0]
-         # Initialization
+        ex_ve_f, in_ve_f, to_ho_f, bo_ho_f, in_ho_f, ex_in_f, in_in_f, ex_ve_a, in_ve_a, to_ho_a, bo_ho_a, in_ho_a, ex_in_a, in_in_a = CellComplexDecompose.processItem(c)
         st.session_state['topology'] = c
 if c:
     st.subheader(c)
@@ -249,7 +249,6 @@ if c:
     #faces = []
     #_ = c.Faces(None, faces)
     north = [0,1,0]
-    ex_ve_f, in_ve_f, to_ho_f, bo_ho_f, in_ho_f, ex_in_f, in_in_f, ex_ve_a, in_ve_a, to_ho_a, bo_ho_a, in_ho_a, ex_in_a, in_in_a = CellComplexDecompose.processItem(c)
     #ex_ve_f, in_ve_f, to_ho_f, bo_ho_f, in_ho_f, ex_in_f, in_in_f, ex_ve_a, in_ve_a, to_ho_a, bo_ho_a, in_ho_a, ex_in_a, in_in_a = CellComplexDecompose.processItem(None)
     
     if ex_ve_f_f:
